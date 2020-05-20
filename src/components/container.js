7758dbaed94ff2fdeb6dfdf805ee35f64b37b693
Vishal Gupta <ims.vishal@gmail.com>
2020-05-20 20:24:38 +0530

    Removed react-router

--- src/components/container.js
+++ src/components/container.js
@@ -1,27 +1,12 @@
 import React from "react";
-import {
-  Route,
-  BrowserRouter as Router,
-  Switch,
-  Redirect
-} from "react-router-dom";
 import Offering from "./pages/offering";
 import "../static/styles/container.scss";
 
 const Container = () => {
   return (
-    <Router>
-      <div className="container-fluid">
-        <Switch>
-          <Route path="/offering">
-            <Offering />
-          </Route>
-          <Route path="/">
-            <Redirect to="/offering" />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <div className="container-fluid">
+      <Offering />
+    </div>
   );
 };
 
